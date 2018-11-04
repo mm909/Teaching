154eccac9a8e887383ed5baefd83d84bd009e1a9
mm909 <Bobar312@gmail.com>
2018-11-04 01:26:15 -0700

    Updated code for p5 p1

--- projects/introp5/project1/code/sketch.js
+++ projects/introp5/project1/code/sketch.js
@@ -3,9 +3,6 @@
 
 var x = 0;
 var y = 0;
-var r = 100;
-var g = 255;
-var b = 100;
 
 function setup() {
   createCanvas(500, 500);
@@ -14,10 +11,7 @@ function setup() {
 function draw() {
   background(151);
   x = mouseX;
-  fill(r, g, b)
-  // noFill();
-  noStroke();
-  // stroke(255);
-  // strokeWeight(5);
-  ellipse(x, height / 2, 20, 20)
+  y = mousey;
+  fill(100, 255, 100)
+  ellipse(x, y, 20, 20)
 }
